87ac93ee315738b222dbd13d22e6ff921383fdae
sejan001 <sejansejan2019@gmail.com>
2024-07-12 11:09:55 +0545

    p

--- lib/feature/products/Products/Bloc/products_bloc.dart
+++ lib/feature/products/Products/Bloc/products_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_bloc/feature/products/Products/Bloc/products_events.dart';
 import 'package:project_bloc/feature/products/Products/Bloc/products_state.dart';
@@ -5,7 +7,8 @@ import 'package:project_bloc/feature/products/domain/model/cart_model.dart'
     as C;
 import 'package:project_bloc/feature/products/domain/repo/product_repo.dart';
 import 'package:project_bloc/feature/products/domain/model/product_model.dart';
-import 'package:project_bloc/feature/products/presentation/bloc/carts_bloc.dart';
+import 'package:project_bloc/feature/products/domain/services/shared_prefereneces_service.dart';
+
 import 'package:project_bloc/feature/products/presentation/cubit/search_products_cubit.dart';
 
 class ProductsBloc extends Bloc<ProductsEvents, ProductsState> {
@@ -13,7 +16,7 @@ class ProductsBloc extends Bloc<ProductsEvents, ProductsState> {
   ProductsBloc(this.searchProductsCubit) : super(ProductsInitial()) {
     on<FetchProducts>(_onFetchProducts);
 
-    // on<AddProduct>(_addProduct);
+    on<AddProduct>(_addProduct);
     // on<DeleteProduct>(_deleteProduct);
   }
 }
@@ -27,7 +30,7 @@ Future<void> _onFetchProducts(
     final List<Products> products = await ProductsRepository().fetchProducts(
       model: event.filterModel,
     );
-    print("filter hoo ${event.filterModel.searchQuery}");
+  
     emit(ProductsLoaded(products: products));
   } catch (e) {
     emit(ProductsError("Failed to fetch products: $e"));
@@ -35,15 +38,15 @@ Future<void> _onFetchProducts(
 }
 
 
-// Future<void> _addProduct(AddProduct event, Emitter<ProductsState> emit) async {
-//   try {
-//     final product = await ProductsRepository().addProduct(
-//         event.title.toString(),
-//         event.category,
-//         event.description,
-//         event.thumbnail);
-//   } catch (e) {}
-// }
+Future<void> _addProduct(AddProduct event, Emitter<ProductsState> emit) async {
+  try {
+    final product = await ProductsRepository().addProduct(
+        event.title.toString(),
+        event.category,
+        event.description,
+        event.thumbnail);
+  } catch (e) {}
+}
 
 // Future<void> _deleteProduct(
 //     DeleteProduct event, Emitter<ProductsState> emit) async {
--- lib/feature/products/presentation/screen/home_screen.dart
+++ lib/feature/products/presentation/screen/home_screen.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:line_icons/line_icon.dart';
+
 
 import 'package:project_bloc/feature/products/Products/Bloc/products_bloc.dart';
 import 'package:project_bloc/feature/products/Products/Bloc/products_events.dart';
+import 'package:project_bloc/feature/products/domain/model/cart_model.dart';
 
 import 'package:project_bloc/feature/products/domain/model/filter_product_state_model.dart';
 import 'package:project_bloc/feature/products/domain/model/user_model.dart';
@@ -39,7 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   static List<Widget> _pages = [];
-
+List<Product> product = [];
   @override
   void initState() {
     super.initState();
@@ -75,79 +77,83 @@ class _HomeScreenState extends State<HomeScreen> {
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: Text("Limit and skip"),
-          content: Container(
-            height: 200,
-            width: double.maxFinite,
-            child: Column(
-              children: [
-                Row(
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text("Limit and skip"),
+              content: Container(
+                height: 200,
+                width: double.maxFinite,
+                child: Column(
                   children: [
-                    Text("Limit"),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        controller: _limitController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter limit',
+                    Row(
+                      children: [
+                        Text("Limit"),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: TextField(
+                            controller: _limitController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter limit',
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
                         ),
-                        keyboardType: TextInputType.number,
-                      ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Text("Skip"),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: TextField(
+                            controller: _skipController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter skip',
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Text("Skip"),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        controller: _skipController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter skip',
-                        ),
-                        keyboardType: TextInputType.number,
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    final int limit = int.tryParse(_limitController.text) ?? 10;
+                    final int skip = int.tryParse(_skipController.text) ?? 0;
+                    context.read<FilterProductsCubit>().updateLimit(limit);
+                    context.read<FilterProductsCubit>().updateSkip(skip.toString());
+                    FilterProductStateModel filterState =
+                        context.read<FilterProductsCubit>().state;
+                    if (_limitController.text == null ||
+                        _limitController.text.isEmpty) {
+                      _limitController.text = 0.toString();
+                    }
+                    if (_skipController.text == null ||
+                        _skipController.text.isEmpty) {
+                      _skipController.text = 0.toString();
+                    }
+            
+                    BlocProvider.of<ProductsBloc>(context).add(FetchProducts(
+                      filterModel: filterState.copyWith(
+                        searchQuery: _productNameController.text,
+                        limit: _limitController.text,
+                        skip: _skipController.text,
                       ),
-                    ),
-                  ],
+                    ));
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Apply"),
                 ),
               ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                final int limit = int.tryParse(_limitController.text) ?? 10;
-                final int skip = int.tryParse(_skipController.text) ?? 0;
-                context.read<FilterProductsCubit>().updateLimit(limit);
-                context.read<FilterProductsCubit>().updateSkip(skip.toString());
-                FilterProductStateModel filterState =
-                    context.read<FilterProductsCubit>().state;
-                if (_limitController.text == null ||
-                    _limitController.text.isEmpty) {
-                  _limitController.text = 0.toString();
-                }
-                if (_skipController.text == null ||
-                    _skipController.text.isEmpty) {
-                  _skipController.text = 0.toString();
-                }
-
-                BlocProvider.of<ProductsBloc>(context).add(FetchProducts(
-                  filterModel: filterState.copyWith(
-                    searchQuery: _productNameController.text,
-                    limit: _limitController.text,
-                    skip: _skipController.text,
-                  ),
-                ));
-                Navigator.of(context).pop();
-              },
-              child: Text("Apply"),
-            ),
-          ],
+            );
+          }
         );
       },
     );
@@ -275,83 +281,96 @@ class _HomeScreenState extends State<HomeScreen> {
     showDialog(
         context: context,
         builder: (context) {
-          return AlertDialog(
-            title: Text("Add Products"),
-            content: Container(
-              height: height * .6,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+          return StatefulBuilder(
+            builder: (context, setState) {
+              return AlertDialog(
+                title: Text("Add Products"),
+                content: Container(
+                  height: height * .5 ,
+                  child: Column(
                     children: [
-                      CircleAvatar(
-                        backgroundImage:
-                            _image != null ? FileImage(_image!) : null,
-                        radius: 40,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                   Container(
+                    height: height*.16,
+                    width: width*.3,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      image: DecorationImage(image: _image != null ? FileImage(File(_image!.path)): NetworkImage("https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg"),fit: BoxFit.cover)),),
+                          SizedBox(
+                            width: width * .015,
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                Future<void> imagePicker() async {
+                                  final pickedFile = await _picker.pickImage(
+                                      source: ImageSource.gallery);
+                                  if (pickedFile != null) {
+                                    setState(() {
+                                      _image = File(pickedFile.path);
+                                    });
+                                  }
+                                }
+              
+                                imagePicker();
+                              },
+                              icon: Icon(Icons.add_a_photo_outlined))
+              
+                        ],
                       ),
                       SizedBox(
-                        width: width * .015,
+                        height: height * .015,
                       ),
-                      IconButton(
-                          onPressed: () {
-                            Future<void> imagePicker() async {
-                              final pickedFile = await _picker.pickImage(
-                                  source: ImageSource.gallery);
-                              if (pickedFile != null) {
-                                setState(() {
-                                  _image = File(pickedFile.path);
-                                });
-                              }
-                            }
-
-                            imagePicker();
-                          },
-                          icon: Icon(Icons.add_a_photo_outlined))
+                      TextField(
+                        controller: _titleController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            hintText: "Enter product's title"),
+                      ),
+                      SizedBox(
+                        height: height * .015,
+                      ),
+                      TextField(
+                        controller: _priceController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            hintText: "Enter product's price"),
+                      ),
+                      SizedBox(
+                        height: height * .015,
+                      ),
+                      TextField(
+                        controller: _descriptionController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            hintText: "describe your product"),
+                      ),
+                      SizedBox(
+                        height: height * .015,
+                      ),
+                      TextField(
+                        controller: _categoryController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            hintText: "Enter product's category"),
+                      ),
+                      SizedBox(
+                        height: height * .015,
+                      ),
+                      TextButton(onPressed: (){
+                        BlocProvider.of<ProductsBloc>(context).add(AddProduct(title: _titleController.text,price: _priceController.text, description: _descriptionController.text,
+                        category: _categoryController.text,thumbnail: _image != null ? _image!.path.toString() : ''));
+                      }, child: Text("Add Products",style: TextStyle(color: Colors.orange),))
                     ],
                   ),
-                  SizedBox(
-                    height: height * .015,
-                  ),
-                  TextField(
-                    controller: _titleController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        hintText: "Enter product's title"),
-                  ),
-                  SizedBox(
-                    height: height * .015,
-                  ),
-                  TextField(
-                    controller: _priceController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        hintText: "Enter product's price"),
-                  ),
-                  SizedBox(
-                    height: height * .015,
-                  ),
-                  TextField(
-                    controller: _descriptionController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        hintText: "describe your product"),
-                  ),
-                  SizedBox(
-                    height: height * .015,
-                  ),
-                  TextField(
-                    controller: _categoryController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        hintText: "Enter product's category"),
-                  )
-                ],
-              ),
-            ),
+                ),
+              );
+            }
           );
         });
     return SizedBox.shrink();
